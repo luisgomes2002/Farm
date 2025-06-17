a6165c7376247b7f76267fa44cbfac6b65201e2a
paulo-sbrandao <paulovt.brandao@gmail.com>
2025-06-17 14:21:52 -0300

    Atualizar o PainelIrrigacao.jsx

--- client/src/pages/PainelIrrigacao.jsx
+++ client/src/pages/PainelIrrigacao.jsx
@@ -118,6 +118,7 @@ export default function PainelIrrigacao() {
             </svg>
             <span>
               <strong>Última Leitura:</strong>{" "}
+//
               {ultimaLeitura ? new Date(ultimaLeitura).toLocaleString() : "N/A"}
             </span>
           </div>
